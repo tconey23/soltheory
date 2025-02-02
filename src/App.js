1db3cfd8d7fe18bb6a53d43a0732f5c26b175645
Tom Coney <tom058715@gmail.com>
2025-02-02 09:25:52 -0700

    adds gif to homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './assets/soltheorylogo.png';
+import HomePageLogo from './HomePageLogo';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <HomePageLogo />
         <p>
           Under construction. Check back in later
         </p>
---
+++ src/HomePageLogo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HomePageLogo = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-900">
+    <img 
+        src="/SOLTheory.gif" 
+        alt="Animated GIF" 
+        className="w-64 h-auto rounded-lg shadow-lg"
+      />
+  </div>
+  )
+}
+
+export default HomePageLogo
