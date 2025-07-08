9cd52518f6b35786d29d5bfb34982bac2bfb1388
Tom Coney <tom058715@gmail.com>
2025-07-08 17:18:11 -0600

    updating intersection observer on the homepage menu

--- src/ui_elements/HomePageMenu.jsx
+++ src/ui_elements/HomePageMenu.jsx
@@ -9,30 +9,50 @@ import Login from '../components/Login';
 import Affiliates from '../components/Affiliates';
 import GameData from '../components/game_data/GameData';
 import ThriveParenting from '../components/ThriveParenting';
+import { useInView } from 'react-intersection-observer';
 
 const MotionStack = motion(Stack);
 
-const MenuGridItem = ({ delay = 0, content, index}) => {
+const MenuGridItem = ({ delay = 0, content, index }) => {
+
+  const [itemOpac, setItemOpac] = useState(0)
+
+  const { ref, inView, entry } = useInView({
+    triggerOnce: false,
+    threshold:  Array.from({ length: 101 }, (_, i) => i / 100),
+  });
+  
+  useEffect(() => {
+    if (entry) {
+      setItemOpac(entry.intersectionRatio)
+    }
+  }, [entry, index]);
+
   return (
-    <MotionStack
-      key={index}
-      justifyContent={'center'}
-      alignItems={'center'}
-      overflow={'hidden'}
-      width={'100%'}
-      height={'450px'}
-      bgcolor={'#80808073'}
-      borderRadius={5}
-      initial={{ y: 50, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-      transition={{
-        duration: 0.5,
-        ease: 'easeOut',
-        delay: delay,
-      }}
-    >
-        {content}
-    </MotionStack>
+    <div ref={ref} style={{ width: '100%', height: '450px' }}>
+      
+        <MotionStack
+          // sx={{opacity: itemOpac}}
+          key={index}
+          justifyContent={'center'}
+          alignItems={'center'}
+          overflow={'hidden'}
+          width={'100%'}
+          height={'450px'}
+          bgcolor={'#80808073'}
+          borderRadius={5}
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: itemOpac }}
+          transition={{
+            duration: 0.5,
+            ease: 'easeOut',
+            delay: 0,
+          }}
+        >
+          {inView && content}
+        </MotionStack>
+      
+    </div>
   );
 };
 
