ec88c6dd8087c423783f25297c97dcdd9cc994d9
tconey23 <145934889+tconey23@users.noreply.github.com>
2025-08-02 09:34:48 -0600

    update add prompts

--- src/views/Error.jsx
+++ src/views/Error.jsx
@@ -7,8 +7,19 @@ import { motion } from 'framer-motion';
 const MotionStack = motion(Stack)
 
 const Error = () => {
+    const [seconds, setSeconds] = useState(10)
     const loc = useLocation()
     const navTo = useNavigate()
+
+    useEffect(() => {
+      if(seconds >=1){
+        setTimeout(() => {
+          setSeconds(prev => prev-1)
+        }, 1000);
+      } else {
+        navTo('/home')
+      }
+    }, [seconds])
 
   return (
     <Stack direction={'column'} width={'100%'} height={'100%'} sx={{bgcolor: '#f4f6f8',}} justifyContent={'center'}>
@@ -16,14 +27,21 @@ const Error = () => {
       <Typography fontFamily={'fredoka regular'} sx={{bgcolor: '#f4f6f8',}}>This page doesn't exist</Typography>
       
       <MotionStack
+        justifyContent={'center'}
+        alignItems={'center'}
+        width={'100%'}
+        height={'100%'}
         initial={{opacity: 0}}
         animate={{opacity: 1}}
         transition={{duration: 2, delay: 1}}
       >
-      <motion.img  
+      <motion.img
+        width="50%"  
+        height='auto'
         src='/soltheorygrayscale.png'
         />
       <Typography fontFamily={'fredoka regular'} fontSize={20} sx={{bgcolor: '#f4f6f8',}}>Don't worry, this happens sometimes.</Typography>
+      <Typography fontFamily={'fredoka regular'} fontSize={20} sx={{bgcolor: '#f4f6f8',}}>{`Redirecting in ${seconds}`}</Typography>
       <Stack>
         <Button onClick={() => navTo('/home')}>Home</Button>
       </Stack>
